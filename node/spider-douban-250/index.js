dff4ca9b4cfc287be5b8130594d48ee19dabee0a
gxz2019 <1173017852@qq.com>
2019-10-26 16:26:46 +0800

    s

---
+++ node/spider-douban-250/index.js
@@ -0,0 +1,59 @@
+const https = require('https');
+const http = require('http');
+const cheerio = require('cheerio');
+
+// doubanSpider('https://movie.douban.com/top250?start=25&filter=',(data) => {
+//     console.log(data)
+// })
+function doubanSpider(url,cb){
+    https.get(url, (res) => {
+    let html = '';
+    //以流的形式传出
+    res.on('data', (chunk) => {
+        html += chunk;
+    })
+    res.on('end', () => {
+        // console.log(html)
+        const $ = cheerio.load(html);
+        let movies = [];
+        $('li .item').each(function () {
+            //拿到每一个电影
+            const picUrl = $('.pic img', this).attr('src');
+            const title = $('.info .title', this).text();
+            const star = $('.star .rating_num', this).text();
+            const inq = $('.inq', this).text();
+            // console.log({ picUrl })
+            // console.log({ title })
+            // console.log({ star })
+            // console.log({ inq })
+            movies.push({
+                picUrl,
+                title,
+                star,
+                inq
+            })
+        })
+        cb(movies)
+
+    })
+})
+}
+
+
+http.createServer((req,res)=>{
+    doubanSpider('https://movie.douban.com/top250?start=25&filter=',(data) =>{
+        //text/plain 纯文本
+        //text/html html
+        //mime 类型
+        console.log('data',data)
+        res.writeHead(200,{
+            'Content-type':'application/json',
+            'Access-Control-Allow-origin':'*'
+        })
+        res.end(JSON.stringify(data))//把一个对象变成字符串
+    })
+    
+}).listen(3000,() => {
+    console.log('server is running 3000')
+})
+
