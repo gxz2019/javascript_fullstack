3b48f1115e62c588a2a62b02dc4e1cb34e7d5bc4
gxz2019 <1173017852@qq.com>
2019-10-30 17:10:37 +0800

    vue学习

---
+++ NOTE/test5/gxz.js
@@ -0,0 +1,13 @@
+// var f = function (x) {
+//     return x * x;
+// };
+
+// var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// var result = [];
+// for (var i=0; i<arr.length; i++) {
+//     result.push(f(arr[i]));
+// }
+// console.log(result)
+let s1 = Symbol('31321')
+console.log(typeof(s1))
+console.log(s1)
---
+++ vue/vue-router/src/components/mine.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mine">这事我的页面
+      <!-- <router-link :to="{name:'Detail',params:{id:123}}" ><button class="go-detail">查看详情</button></router-link>
+       -->
+       <button class="go-detail" @click="a">查看详情</button>
+      <router-view></router-view>   
+  </div>
+  
+</template>
+
+<script>
+export default {
+    methods:{
+        a(){
+            this.$router.push({name:'Detail',params:{id:234}})
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
