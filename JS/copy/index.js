97fbb41d90374550fbb3b073d23a8274e64acfbf
gxz2019 <1173017852@qq.com>
2019-11-08 19:46:41 +0800

    JSxuexi

---
+++ JS/copy/index.js
@@ -0,0 +1,20 @@
+// // let a = 'wn'
+// // let b = a
+// // a = 'sss'
+
+
+// // console.log(b)
+// let a = {
+//     name:'wn',
+//     book:{
+//         title:'You Dont Know Js',
+//         price:'45'
+//     }
+// }
+// let b = a
+// a.name = 'woniu'
+// a.book.price = '55'
+// console.log(b)
+
+let a = [1,'1',[2,3]]
+let b = a.slice()
