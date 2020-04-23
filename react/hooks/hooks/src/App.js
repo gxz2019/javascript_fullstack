aaa1c231edb6a975618aabc94671e995059d9659
gxz2019 <1173017852@qq.com>
2020-04-23 22:29:22 +0800

    hooks

--- react/hooks/hooks/src/App.js
+++ react/hooks/hooks/src/App.js
@@ -1,17 +1,30 @@
-import React,{useState, useEffect} from 'react';
+import React,{useState,createContext,useContext} from 'react';
+
+
+function B() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+const value = useContext(B)
 
 function App(props) {
   const [count,setCount] = useState(0)
   function changeCount() {
     setCount(count+1)
   }
-  useEffect(() => {
-    console.log(`Effect${count}`)
-  })
+  // useEffect(() => {
+  //   console.log(`Effect${count}`)
+  // },[count])
   return (
     <div>
       <h1>{count}</h1>
       <button onClick={changeCount}>click</button>
+      <B.Provider >
+        
+      </B.Provider>
     </div>
   )
 }
